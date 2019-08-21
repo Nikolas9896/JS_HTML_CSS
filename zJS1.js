bf6bed1c23693af8743fa8389220db591c9f208e
Nikolas9896 <nhlynka@gmail.com>
2019-08-21 13:32:27 +0300

    add prompt for Surname and age. Flush it all into console and show alert

--- zJS1.js
+++ zJS1.js
@@ -1,4 +1,8 @@
 alert("Hi there from JS FILE!");
-var userName = prompt("What is your name?");
+var userName    = prompt("What is your name?");
+var userSurname = prompt("What is your surname?");
+var userAge     = prompt("How old are you?");
 alert("Nice to meet you, " + userName);
-console.log("Also great to meet wit you " + userName);
+alert("You full name is: " + userName + " " + userSurname);
+console.log("Also great to meet with you " + userName);
+console.log("And I know your age. You are: " + userAge + " years old!");
