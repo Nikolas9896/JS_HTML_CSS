f1fa6a7de5bd662facf48ea0c452e135a93627af
Nikolas9896 <nhlynka@gmail.com>
2019-08-21 17:00:29 +0300

    add Guess Game with all logic and isNaN checking

--- zJS1.js
+++ zJS1.js
@@ -1,3 +1,5 @@
+
+// User's introduce
 alert("Hi there from JS FILE!");
 var userName    = prompt("What is your name?");
 var userSurname = prompt("What is your surname?");
@@ -7,4 +9,24 @@ alert("You full name is: " + userName + " " + userSurname);
 console.log("Also great to meet with you " + userName);
 console.log("And I know your age. You are: " + userAge + " years old!");
 
+// Age Calculator
 alert("You are alive in a planet: " + userAge*365.25 +" days!");
+
+//Guess Game
+var guessNumber = 30;
+var enterNumber = Number(prompt("Guess Number: "));
+while(guessNumber !== enterNumber)
+{
+    if(isNaN(enterNumber))
+    {
+        enterNumber = Number(prompt("Please enter NUMBER only: "));
+    }
+    else if (enterNumber < guessNumber)
+    {
+        enterNumber = Number(prompt("Your number is less than ours. Please, guess Number again: "));
+    }
+    else
+    {
+        enterNumber = Number(prompt("Your number is larger than ours. Please, guess Number again: "));
+    }
+}
