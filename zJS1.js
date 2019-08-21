7d62a57e886b5d7b59bfd91717ce6cc52108553f
Nikolas9896 <nhlynka@gmail.com>
2019-08-21 17:41:40 +0300

    add While Loop SET Realization for console.log

--- zJS1.js
+++ zJS1.js
@@ -32,3 +32,37 @@ while(guessNumber !== enterNumber)
 }
 console.log("Silence WIN! First Always!");
 alert("YOU WIN");
+
+//While Loops SET
+var number = -10; // start number;
+console.log("Print all numbers between -10 and 19");
+while(number<20)
+{
+    console.log("number");
+    number++;
+}
+
+console.log("Print all even numbers between 10 and 40");
+number = 10;// start 2 number;
+while(number<41)
+{
+    console.log(number);
+    number+=2;
+}
+console.log("Print all odd numbers between 300 and 333");
+number = 301;// start 2 number;
+while(number<334)
+{
+    console.log(number);
+    number+=2;
+}
+console.log("Print all odd numbers between 300 and 333");
+number = 15;// start 2 number;
+while(number<51)
+{
+    if(!(number % 5) && !(number % 3))
+    {
+        console.log(number);
+    }
+    number+=3;
+}
