fc0414989ea861c08ed8d73a96f3e2b5dd685e10
Nikolas9896 <nhlynka@gmail.com>
2019-08-23 18:27:00 +0300

    add delete option in TODO LIST application

--- toDoList.js
+++ toDoList.js
@@ -10,12 +10,19 @@ console.log("WELCOME to TODO LIST!");
 var todos = [];
 //ask what to do with help tip
 var input = prompt("What would you like to do? type \"help\" for information:");
+var index = -1;
 while(input !=="quit")
 {   //ask what to do
    
     if(input === "list")
     {
-        console.log(todos);
+        console.log("*******************");   
+        todos.forEach(function(item, index)
+        {
+            console.log(index + " "+ item);    
+        });
+        console.log("*******************");
+        
     }
     else if (input ==="new")
     {
@@ -23,9 +30,18 @@ while(input !=="quit")
         var newTodo = prompt("Enter new todo");
         //add to todos array
         todos.push(newTodo);
+        console.log("New todo's item is added!");
+    }
+    else if(input === "delete")
+    {
+        //ask for index of todo's item
+       index = prompt("Enter index of todo to delete");
+       //delete one element
+       todos.splice(index, 1);
+       console.log(index + " item is deleted");
     }
     else if(input ==="help")
-    {
+    {   //show help
         console.log("HELP type:");
         console.log("new - Add a Todo");
         console.log("list - List all Todos");
