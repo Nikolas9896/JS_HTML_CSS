ed6ab44e0128036d69f7147a68a2164eaa384a65
Nikolas9896 <nhlynka@gmail.com>
2019-08-23 15:31:53 +0300

    add logic for preview list and add new logic

--- toDoList.js
+++ toDoList.js
@@ -1,1 +1,24 @@
-toDoList.js
+/** Console Application
+ *  Todo List
+
+"new" - Add A Todo
+"list" - View all todos
+"quit" - Quit Application */
+
+console.log("WELCOME to TODO LIST!");
+
+var todos = ["Buy new turtle"];
+//ask what to do.
+var input = prompt("What would you like to do?");
+
+if(input === "list")
+{
+    console.log(todos);
+}
+else if (input ==="new")
+{
+    //ask for new todo
+    var newTodo = prompt("Enter new todo");
+    //add to todos array
+    todo.push(newTodo);
+}
